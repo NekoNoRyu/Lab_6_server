7400dfcb9dd4188d43e6c68bf879a18bbc3c25d2
NekoNoRyu <mmm1aaa1xxx@gmail.com>
2024-03-01 13:28:39 +0300

    a

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -5,7 +5,7 @@ import support.Document;
 
 public class Main {
     public static void main(String[] args) {
-        //all files in program root (collection: src/main/resources/csv2)
+        //all files in program root (collection: src/main/resources/csv)
         if (args.length != 0) {
             Document.setFileWithCollectionPath(args[0]);
             Application.fillMenu();
